74e7302d94fc7c2693ede2f8f2594b6186b1e4f1
Manuel Lorenzo <manulorenzop@gmail.com>
2023-07-28 13:38:11 +0200

    🥅 -- improve error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+# app/controllers/application_controller
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    redirect_to '/errors#not_found'
+  end
 end
