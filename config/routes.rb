111a53b73e7f16922b633668572091c511282b01
Manuel Lorenzo <manulorenzop@gmail.com>
2023-07-28 20:02:12 +0200

    ✨ -- add Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/l/:slug', to: 'links#show', as: :short
+  get 'home/index'
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
+  root to: 'home#index'
 end
