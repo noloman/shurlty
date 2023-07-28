83724913cd700969522213bb25b5d863e3ddacc3
Manuel Lorenzo <manulorenzop@gmail.com>
2023-07-28 20:00:06 +0200

    🥅  -- catch RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# app/controllers/application_controller
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::
+
+  private
+
+  def record_not_found
+    redirect_to '/errors#not_found'
+  end
 end
