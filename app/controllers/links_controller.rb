bea89ea8970a8f9e97ca3fe948b62a3a5a72a01a
Manuel Lorenzo <manulorenzop@gmail.com>
2023-07-29 18:18:02 +0200

    🐛  -- only generate short URL if the URL isn't already in DB

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# app/controllers/LinksController
 class LinksController < ApplicationController
   def show
     @link = Link.find_by_slug(params[:slug])
@@ -5,6 +8,19 @@ class LinksController < ApplicationController
   end
 
   def create
+    @link = Link.find_by_url(params[:url])
+    if @link.nil?
+      create_link
+    else
+      # the URL is already in the DB
+      redirect_to short_url(@link.slug)
+    end
+  end
+
+  private
+
+  def create_link
+    # there's no URL yet in the DB
     @link = Link.new
     @link.url = params[:url]
     @link.slug = @link.generate_slug
