2926743d2cca56392cbbc7f167137498538e0ec7
Manuel Lorenzo <manulorenzop@gmail.com>
2023-07-29 19:18:17 +0200

    🔥 -- write wrong specs

--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -3,7 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Links', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  subject { Link.new(url: 'https://www.google.com', slug: 'aaa') }
+
+  describe 'GET /show' do
+    context 'when showing a link' do
+      it 'should be valid' do
+        expect(subject).to be_valid
+      end
+    end
+  end
+
+  describe 'Shortify' do
+    context 'when creating a link with valid URL and slug' do
+      it 'should be valid' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'when trying to creating a link with an invalid URL' do
+      subject { Link.new(url: 'google.com', slug: 'aaa') }
+      it 'should NOT be valid' do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context 'when trying to creating a link without slug' do
+      subject { Link.new(url: 'google.com', slug: '') }
+      it 'should NOT be valid' do
+        expect(subject).not_to be_valid
+      end
+    end
   end
 end
