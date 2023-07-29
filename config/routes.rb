1083d16616079fd16d7cab9bd241e07d7e360351
Manuel Lorenzo <manulorenzop@gmail.com>
2023-07-29 12:15:20 +0200

    🔒️ -- add privacy policy generated with TermsFeed

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
   root to: 'home#index'
+  get '/privacy_policy', to: redirect('/privacy_policy.html'), as: :privacy_policy
   resources :links
 end
